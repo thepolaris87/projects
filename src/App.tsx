8d49bf21019c8763b199eb1b13ac23f87658ab37
polaris <jinbum33@gmail.com>
2023-03-05 00:56:21 +0900

    update

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./assets/styles/style.css";
+
+function App() {
+  return (
+    <div className="flex">
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+    </div>
+  );
+}
+
+export default App;
