ea20c038c1ea9d1277a3e1370ae02fd9b5595723
polaris <jinbum33@gmail.com>
2023-03-08 21:45:04 +0900

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            height: {
+                chatListWrapper: 'calc(100% - 4rem)',
+                chatListContainer: 'calc(100% - 2.5rem)',
+            },
+            maxHeight: {
+                chatListWrapper: 'calc(100% - 4rem)',
+                chatListContainer: 'calc(100% - 2.5rem)',
+            },
+        },
+    },
+    plugins: [],
+};
